57996f0384bf8b549c6bb9e81c653a5aab25344a
Isabella Fishman <fishman.bellam@gmail.com>
2024-10-26 17:31:47 -0500

    Added nav to view post

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,14 +3,20 @@ import { useNavigate } from 'react-router-dom';
 
 export default function Map() {
     const navigate = useNavigate();
-    const handleClick = () => {
+    const Create_Post = () => {
         navigate('/create_post'); 
+    };
+    const View_Post = () => {
+        navigate('/view_post'); 
     };
 
     return (
         <div>Map
-            <button onClick={handleClick}>
+            <button onClick={Create_Post}>
                 <i className="fa-solid fa-plus"></i>Create
+            </button>
+            <button onClick={View_Post}>
+                View Post X
             </button>
         </div>
     )
